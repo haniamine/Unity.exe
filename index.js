caf6c268faaa13ec34097b01e867c7bf040a9bd0
Amine Hani <HaniAmiine@gmail.com>
2021-03-21 01:25:54 +0000

    add automatic response

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const client = new Discord.Client()
 client.on("ready" , () => console.log(`Logged in as ${client.user.tag}`))
 
 client.on("message", msg => {
-  if (msg.content === "ping"){
-    msg.reply("pong")
+  if (msg.content.includes("salam")){
+    msg.reply("Wa 3alaykomo salaaam")
+  }
+  if (msg.content.includes("whitelist")){
+    h = new Date().getHours()+1
+    let answer = "Whiteliste ON ghir mn 17h tal 22h." 
+    if( h>=17 && h<22 ) answer = "Mer7ba douz lwaiting"
+    if( h>=22 ) answer = "Noud awa jm3 kerek, kan khedmo mn 17h tal 22h." 
+    msg.reply(answer)
   }
 })
 
